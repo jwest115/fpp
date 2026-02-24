bfc2cd43545b6fd5fbb4040ba8bb4b9db0c11d88
Rob Bocchino <bocchino@jpl.nasa.gov>
2026-02-23 19:08:08 -0800

    Revise fpp-to-cpp state machine tests

--- compiler/tools/fpp-to-cpp/test/state-machine/state/run.sh
+++ compiler/tools/fpp-to-cpp/test/state-machine/state/run.sh
@@ -14,7 +14,7 @@ basic_guard()
 
 basic_guard_string()
 {
-  run_test "$fpp_flags",../../StringFwDefs.fpp BasicGuardString && \
+  run_test "$fpp_flags" BasicGuardString && \
     diff_cpp BasicGuardStringStateMachine
 }
 
@@ -62,7 +62,7 @@ basic_self()
 
 basic_string()
 {
-  run_test "$fpp_flags",../../StringFwDefs.fpp BasicString && \
+  run_test "$fpp_flags" BasicString && \
     diff_cpp BasicStringStateMachine
 }
 
